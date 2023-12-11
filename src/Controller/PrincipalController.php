3c88a73791d5e052959ff5313d4094db7d16e3ea
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-11 11:03:09 +0100

    homepage

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -6,13 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PrincipalController extends AbstractController
-{
-    #[Route('/principal', name: 'app_principal')]
-    public function index(): Response
-    {
-        return $this->render('principal/index.html.twig', [
-            'controller_name' => 'PrincipalController',
-        ]);
+class PrincipalController extends AbstractController {
+
+    #[Route('/homepage', name: 'homepage')]
+    public function index(): Response {
+        return $this->render('principal/home.html.twig');
     }
 }
