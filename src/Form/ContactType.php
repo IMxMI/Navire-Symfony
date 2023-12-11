0670a46d04dd97eec8fda6ba56d7a3504074a14c
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-11 12:57:00 +0100

    formulaire type

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,22 +6,30 @@ use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Validator\Constraints\Email;
+use Doctrine\DBAL\Types\DateTimeType;
 
-class ContactType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class ContactType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Mail')
-            ->add('Message')
-            ->add('dateMessage')
+                ->add('Nom', TextType::class, [
+                    'required' => true,
+                ])
+                ->add('Prenom', TextType::class, [
+                    'required' => true,
+                ])
+                ->add('Mail', TextType::class, [
+                    'required' => true,
+                ])
+                ->add('Message', TextType::class, [
+                    'required' => true,
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Message::class,
         ]);
