7a8b7ca02d700c586dde1d7680a14c046302c5a0
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-11 12:40:30 +0100

    Create ContactType.php

---
+++ src/Form/ContactType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Nom')
+            ->add('Prenom')
+            ->add('Mail')
+            ->add('Message')
+            ->add('dateMessage')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
