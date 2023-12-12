6f4dc53d9f6c974b9c65c3c17bcfbb5cfc1cc147
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-12 22:06:57 +0100

    envoie de mail + enregistrement bdd

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactType;
+use App\Service\GestionContact;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Message;
 
-class MessageController extends AbstractController
-{
-    #[Route('/message', name: 'app_message')]
-    public function index(): Response
-    {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
+class MessageController extends AbstractController {
+
+    #[Route('/contact', name: 'demandeContact')]
+    public function contact(Request $request, GestionContact $gestionContact): Response {
+        $message = new Message();
+        $form = $this->createForm(ContactType::class, $message);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = $form->getData();
+            $gestionContact->creerContact($message);
+            $gestionContact->envoieMailContact($message);
+            return $this->redirectToRoute("homepage");
+        }
+        return $this->render('message/demandeContact.html.twig', [
+                    'form' => $form->createView(),
         ]);
     }
 }
